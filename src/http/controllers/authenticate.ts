a6b7f440e92e9b138cf7e4d5636bc05b9e30c5d4
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2023-12-03 11:58:32 -0300

    test: authenticate usecase

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,35 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { AuthenticateUseCase } from '@/user-cases/authenticate'
+import { InvalidCredentialsError } from '@/user-cases/errors/invalid-credentials-error'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const authenticateseCase = new AuthenticateUseCase(usersRepository)
+
+    await authenticateseCase.execute({
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    throw error
+  }
+
+  return reply.status(200).send()
+}
