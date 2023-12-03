c1e434a46ab927227b740c16087ef92163e1850f
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2023-12-03 16:26:23 -0300

    feat: check in usecase

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,4 +1,5 @@
 import { Prisma, User } from '@prisma/client'
+import crypto from 'node:crypto'
 import { usersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements usersRepository {
@@ -26,7 +27,7 @@ export class InMemoryUsersRepository implements usersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: crypto.randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
