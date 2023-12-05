bbb3cf3734e532f706c71d183870483dfbe62346
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2023-12-04 23:40:23 -0300

    feat: search gyms usecase

--- src/use-cases/search-gyms.spec.ts
+++ src/use-cases/search-gyms.spec.ts
@@ -13,7 +13,7 @@ describe('Search Gyms Use Case', () => {
 
   it('should be able to search for gyms', async () => {
     await gymsRepository.create({
-      title: 'Javascript Gym',
+      title: 'JavaScript Gym',
       description: null,
       phone: null,
       latitude: -21.1194368,
@@ -29,11 +29,34 @@ describe('Search Gyms Use Case', () => {
     })
 
     const { gyms } = await sut.execute({
-      query: 'Javascript',
+      query: 'JavaScript',
       page: 1,
     })
 
     expect(gyms).toHaveLength(1)
-    expect(gyms).toEqual([expect.objectContaining({ title: 'Javascript Gym' })])
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JavaScript Gym' })])
+  })
+
+  it('should be able to fetch paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `JavaScript Gym ${i}`,
+        description: null,
+        phone: null,
+        latitude: -21.1194368,
+        longitude: -42.9359104,
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'JavaScript Gym 21' }),
+      expect.objectContaining({ title: 'JavaScript Gym 22' }),
+    ])
   })
 })
