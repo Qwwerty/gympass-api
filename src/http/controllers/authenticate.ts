b55423d88e3d0108f1907df5662078fa66aa1f80
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2023-12-05 17:33:59 -0300

    feat: jwt fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,5 +1,5 @@
-import { InvalidCredentialsError } from '@/user-cases/errors/invalid-credentials-error'
-import { makeAuthenticate } from '@/user-cases/factories/make-authenticate-use-case'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticate } from '@/use-cases/factories/make-authenticate-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -17,10 +17,21 @@ export async function authenticate(
   try {
     const authenticateseCase = makeAuthenticate()
 
-    await authenticateseCase.execute({
+    const { user } = await authenticateseCase.execute({
       email,
       password,
     })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
@@ -28,6 +39,4 @@ export async function authenticate(
 
     throw error
   }
-
-  return reply.status(200).send()
 }
